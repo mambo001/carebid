21a91fd5dbca3ca1b0476caee7bd34acbfe3ccf4
Reuben <reuben.gorda@gmail.com>
2026-04-25 14:49:04 +0800

    chore: remove test file

--- apps/backend/test_handler.ts
+++
@@ -1,5 +0,0 @@
-import { HttpRouter, HttpServerResponse } from "@effect/platform"
-import { Effect } from "effect"
-
-// Test what type HttpRouter.get expects
-const handler = HttpRouter.get("/test", Effect.succeed(HttpServerResponse.text("ok")))
